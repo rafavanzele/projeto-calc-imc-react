2fd52a014fe0494690819c4ba8f37e7bcc9d45cb
rafavanzele <rafa_vanzele@hotmail.com>
2024-06-05 17:31:47 -0300

    primeiros passos calc imc react

---
+++ CALC_IMC_REACT/calc_imc/src/App.jsx
@@ -0,0 +1,17 @@
+// COMPONENTS
+import ImcCalc from './components/ImcCalc'
+// ESTILOS
+import './App.css'
+
+
+function App() {
+  
+
+  return (
+    <div className='container'>
+      <ImcCalc/>
+    </div>
+  )
+}
+
+export default App
---
+++ CALC_IMC_REACT/calc_imc/src/components/ImcCalc.jsx
@@ -0,0 +1,31 @@
+import './ImcCalc.css'
+
+const ImcCalc = () => {
+  return (
+    <div id="calc-container">
+        <h2>Calculadora de IMC</h2>
+
+        <form id='imc-form'>
+            <div className="form-inputs">
+                <div className="form-control">
+                    <label htmlFor="height">Altura:</label>
+                    <input type="text" name='height' id='height' placeholder='Exemplo: 1,80' />
+                </div>
+
+                <div className="form-control">
+                    <label htmlFor="weight">Peso:</label>
+                    <input type="text" name='weight' id='weight' placeholder='Exemplo: 75' />
+                </div>
+            </div>
+
+            
+            <div className="action-control">
+                <button>Calcular</button>
+                <button>Limpar</button>
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default ImcCalc
